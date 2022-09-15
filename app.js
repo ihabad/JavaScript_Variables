25f695a92e22419b4caa3d20b45ebd1a49656e40
ihababoudiab@gmail.com <ihababoudiab@gmail.com>
2022-09-15 08:25:14 -0700

    first commit

--- app.js
+++ app.js
@@ -8,3 +8,28 @@ console.log(
 // Exercise 2
 
 // Exercise 3
+const firstName = "Ihab";
+let lastName = "AbouDiab";
+var age = "29";
+
+lastName = "Bob";
+age = "40";
+
+let language ="JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let OnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let selectedSize = 'M';
+console.log('${price}  ${OnSale}; ${salePercentage} with ${stock} in stock');
+
+let title = "Name of the wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead= true;
+
+console.log('${title} the ${author}; ${pageCount} with ${bookmark} {hasRead}');
